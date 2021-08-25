79e113438cedbd28fab3adc73a895c319e0db0d3
Charlie Allen <charlie.allenjr@gmail.com>
2021-08-25 16:25:56 -0600

    Inital go

---
+++ webhook.py
@@ -0,0 +1,11 @@
+import requests 
+import json
+
+webhook_url = 'http://127.0.0.1:5000/webhook'
+
+data = {
+		'name':'brad',
+		'Channel':'test url'
+		}
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
